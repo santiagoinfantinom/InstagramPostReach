f0ee003c49c2c84847c8b4f5f43c937e83d513ca
santiagoinfantinom <45784254+santiagoinfantinom@users.noreply.github.com>
2023-05-15 16:28:26 +0200

    Add files via upload

---
+++ InstagramPostReach.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import PassiveAggressiveRegressor
+
+data = pd.read_csv("Instagram.csv", encoding = 'latin1')
+print(data.head())
+
+home = data["From Home"].sum()
+hashtags = data["From Hashtags"].sum()
+explore = data["From Explore"].sum()
+other = data["From Other"].sum()
+
+labels = ['From Home','From Hashtags','From Explore','Other']
+values = [home, hashtags, explore, other]
+
+data = data.dropna()
+plt.figure(figsize=(10, 8))
+plt.style.use('fivethirtyeight')
+plt.title("Distribution of Impressions From Home")
+sns.distplot(data['From Home'])
+#plt.show()
+
+plt.figure(figsize=(10, 8))
+plt.title("Distribution of Impressions From Hashtags")
+sns.distplot(data['From Hashtags'])
+plt.show()
+
+plt.figure(figsize=(10,8))
+plt.title("Distribution of Impressions From Explore")
+sns.distplot(data['From Explore'])
+plt.show()
+
+
+
+fig = px.pie(data, values=values, names=labels,
+             title='Impressions on Instagram Posts From Various Sources', hole=0.5)
+fig.show()
+
+"""
+text = " ".join(i for i in data.Caption)
+stopwords = set(STOPWORDS)
+wordcloud = WordCloud(stopwords=stopwords, background_color="white").generate(text)
+plt.style.use('classic')
+plt.figure( figsize=(12,10))
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+#plt.show()
+
+text = " ".join(i for i in data.Hashtags)
+stopwords = set(STOPWORDS)
+wordcloud = WordCloud(stopwords=stopwords, background_color="white").generate(text)
+plt.figure( figsize=(12,10))
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.show()
+"""
+figure = px.scatter(data_frame = data, x="Impressions",
+                    y="Likes", size="Likes", trendline="ols",
+                    title = "Relationship Between Likes and Impressions")
+figure.show()
